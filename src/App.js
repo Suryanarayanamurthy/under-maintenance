2beb21f7a4cdea24c6df0123f88c70d0adcd2a4c
marcelscruz <marcelcruz@live.com>
2019-12-19 15:00:59 +0100

    Add help text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <main>
       <Logo />
       <h1>Under maintenance</h1>
+      <p>
+        Please contact <a href="mailto:help@stokr.io">help@stokr.io</a> for more
+        information.
+      </p>
     </main>
   );
 }
